a16eaeccf7f951cb50ecf9e0c1276e278c849b4e
goyoung2 <42776733+goyoung2@users.noreply.github.com>
2026-08-25 21:17:40 +0900

    feat: register v0.2 developer admin guard

--- src/sdk-entry.ts
+++ src/sdk-entry.ts
@@ -1,11 +1,13 @@
 import app from './index';
 import { registerSdkRoutes } from './sdk';
+import { registerConnectAdminV02Routes } from './connect-admin-v02';
 import { registerConnectRoutes } from './connect';
 import { registerConnectDeviceRoutes } from './connect-device';
 import { registerConnectCliApiRoutes } from './connect-cli-api';
 import { registerConnectCliDistributionRoutes } from './connect-cli-distribution';
 
 registerSdkRoutes(app);
+registerConnectAdminV02Routes(app);
 registerConnectRoutes(app);
 registerConnectDeviceRoutes(app);
 registerConnectCliApiRoutes(app);
