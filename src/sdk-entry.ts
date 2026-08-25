d7973c2a245b3fe99d3feb9bf3792ae076dfcb6d
goyoung2 <42776733+goyoung2@users.noreply.github.com>
2026-08-25 22:14:02 +0900

    feat: register Connect developer and CLI app APIs

--- src/sdk-entry.ts
+++ src/sdk-entry.ts
@@ -2,9 +2,13 @@ import app from './index';
 import { registerSdkRoutes } from './sdk';
 import { registerConnectRoutes } from './connect';
 import { registerConnectCliRoutes } from './connect-cli-routes';
+import { registerConnectCliAppRoutes } from './connect-cli-apps';
+import { registerConnectDeveloperAdminRoutes } from './connect-admin-developers';
 
 registerSdkRoutes(app);
 registerConnectRoutes(app);
 registerConnectCliRoutes(app);
+registerConnectCliAppRoutes(app);
+registerConnectDeveloperAdminRoutes(app);
 
 export default app;
