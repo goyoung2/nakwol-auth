ebe8df9e13afe0863cadeca416a0c9428dc9eada
goyoung2 <42776733+goyoung2@users.noreply.github.com>
2026-08-25 21:11:22 +0900

    feat: register Connect CLI distribution routes

--- src/sdk-entry.ts
+++ src/sdk-entry.ts
@@ -3,10 +3,12 @@ import { registerSdkRoutes } from './sdk';
 import { registerConnectRoutes } from './connect';
 import { registerConnectDeviceRoutes } from './connect-device';
 import { registerConnectCliApiRoutes } from './connect-cli-api';
+import { registerConnectCliDistributionRoutes } from './connect-cli-distribution';
 
 registerSdkRoutes(app);
 registerConnectRoutes(app);
 registerConnectDeviceRoutes(app);
 registerConnectCliApiRoutes(app);
+registerConnectCliDistributionRoutes(app);
 
 export default app;
