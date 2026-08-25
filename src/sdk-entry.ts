00870a565d19331a94f9f261c5332a9a61bdda2f
goyoung2 <42776733+goyoung2@users.noreply.github.com>
2026-08-25 22:06:37 +0900

    feat: register Connect CLI routes

--- src/sdk-entry.ts
+++ src/sdk-entry.ts
@@ -1,8 +1,10 @@
 import app from './index';
 import { registerSdkRoutes } from './sdk';
 import { registerConnectRoutes } from './connect';
+import { registerConnectCliRoutes } from './connect-cli-routes';
 
 registerSdkRoutes(app);
 registerConnectRoutes(app);
+registerConnectCliRoutes(app);
 
 export default app;
