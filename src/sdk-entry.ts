07c6fc0d1349071c1b91d7643809b6a6f2fdd19a
goyoung2 <42776733+goyoung2@users.noreply.github.com>
2026-08-25 21:00:37 +0900

    feat: register Connect CLI control API

--- src/sdk-entry.ts
+++ src/sdk-entry.ts
@@ -2,9 +2,11 @@ import app from './index';
 import { registerSdkRoutes } from './sdk';
 import { registerConnectRoutes } from './connect';
 import { registerConnectDeviceRoutes } from './connect-device';
+import { registerConnectCliApiRoutes } from './connect-cli-api';
 
 registerSdkRoutes(app);
 registerConnectRoutes(app);
 registerConnectDeviceRoutes(app);
+registerConnectCliApiRoutes(app);
 
 export default app;
