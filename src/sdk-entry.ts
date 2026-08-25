ff97d2b3c6f54e14cb4b33bd6c6d351337589049
goyoung2 <42776733+goyoung2@users.noreply.github.com>
2026-08-25 20:52:16 +0900

    feat: register Connect device authorization routes

--- src/sdk-entry.ts
+++ src/sdk-entry.ts
@@ -1,8 +1,10 @@
 import app from './index';
 import { registerSdkRoutes } from './sdk';
 import { registerConnectRoutes } from './connect';
+import { registerConnectDeviceRoutes } from './connect-device';
 
 registerSdkRoutes(app);
 registerConnectRoutes(app);
+registerConnectDeviceRoutes(app);
 
 export default app;
