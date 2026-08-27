73f2c402eb7cededd943a2a0d17c7effecca423b
goyoung2 <42776733+goyoung2@users.noreply.github.com>
2026-08-27 09:37:39 +0900

    chore(data): bump service to v0.5.0

--- services/data/src/domain.ts
+++ services/data/src/domain.ts
@@ -1,4 +1,4 @@
-export const DATA_SERVICE_VERSION = '0.4.0' as const;
+export const DATA_SERVICE_VERSION = '0.5.0' as const;
 export const DATA_SCHEMA_VERSION = 2 as const;
 
 export const DATA_SCOPES = [
